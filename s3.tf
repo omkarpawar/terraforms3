f3aeb7b73caa006b802535ca2d507e8b2e641e23
omkarpawar <omkar111pawar@gmail.com>
2023-03-24 13:55:25 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,33 +1,12 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "us-west-2"
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-0e306788ff2473ccb" // Amazon Linux 2 AMI (HVM), SSD Volume Type
-  instance_type = "t2.micro"
-  key_name      = "example-key"
-
-  vpc_security_group_ids = [aws_security_group.instance.id]
+resource "aws_s3_bucket" "example" {
+  bucket = "example-bucket"
+  acl    = "private"
 
   tags = {
-    Name = "example-instance"
-  }
-}
-
-resource "aws_security_group" "instance" {
-  name_prefix = "example-instance-"
-  
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    Name = "Example Bucket"
   }
 }
