3d71957c62e2ff98c060850830f7e08510afbf4b
omkarpawar <omkar111pawar@gmail.com>
2023-03-24 15:11:12 +0530

    update11

--- s3.tf
+++ s3.tf
@@ -1,12 +1,12 @@
-provider "aws" {
-  region = "us-west-2"
+resource "random_pet" "bucket_name" {
+  length = 2
 }
 
-resource "aws_s3_bucket" "example" {
-  bucket = "examplbucket"
+resource "aws_s3_bucket" "example_bucket" {
+  bucket = "example-${random_pet.bucket_name.id}"
   acl    = "private"
 
   tags = {
-    Name = "my Bucket"
+    Environment = "dev"
   }
 }
